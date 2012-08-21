ebe94787b82955a99f3db797539efdee420f9c8b
Nathaniel Case <Qalthos@gmail.com>
2012-08-21 17:41:22 -0400

    DRY

--- review.js
+++ review.js
@@ -3,94 +3,40 @@ $(function () {
     items = data['items'];
     year = $('span#year').html();
     $('title').html(year + ' Year in Review');
+    // Range of dates valid for this year.
     low = year+'-07-01';
     high = (parseInt(year) + 1)+'-08-31';
-    proj = 0; conf = 0; evnt = 0; hktn = 0; pres = 0;
-    proj_detail = {}; conf_detail = {}; evnt_detail = {}; hktn_detail = {};
-    pres_detail = {};
+    details = {};
+
     $.each(items, function(index, item) {
       if(item['date'] > low && item['date'] < high) {
         type = item['type'].toLowerCase().replace(' ', '_');
         disc = item['discipline'];
         text = item['label'] + ': <a href="' + item['url'] + '">' + item['relationship-detail'] + '</a>';
-        switch(type) {
-          case 'Conference': conf++;
-            if(typeof conf_detail[disc] == 'undefined') {
-              conf_detail[disc] = [];
-            }
-            conf_detail[disc].push(text);
-            break;
-          case 'Event': evnt++;
-            if(typeof evnt_detail[disc] == 'undefined') {
-              evnt_detail[disc] = [];
-            }
-            evnt_detail[disc].push(text);
-            break;
-          case 'Hackathon': hktn++;
-            if(typeof hktn_detail[disc] == 'undefined') {
-              hktn_detail[disc] = [];
-            }
-            hktn_detail[disc].push(text);
-            break;
-          case 'Press': pres++;
-            if(typeof pres_detail[disc] == 'undefined') {
-              pres_detail[disc] = [];
-            }
-            pres_detail[disc].push(text);
-            break;
-          case 'Project Release': proj++;
-            if(typeof proj_detail[disc] == 'undefined') {
-              proj_detail[disc] = [];
-            }
-            proj_detail[disc].push(text);
-            break;
+        if(typeof details[type] == 'undefined') {
+          details[type] = {};
+          details[type]['count'] = 0;
+          details[type]['items'] = {}
         }
+        details[type]['count']++;
+        if(typeof details[type]['items'][disc] == 'undefined') {
+          details[type]['items'][disc] = [];
+        }
+        details[type]['items'][disc].push(text);
       }
     });
-    $('span#conferences').html(conf);
-    $('span#events').html(evnt);
-    $('span#hackathons').html(hktn);
-    $('span#press').html(pres);
-    $('span#projects').html(proj);
-    $.each(conf_detail, function(key, values) {
-      section = '<h3>' + key + '</h3><ul>';
-      $.each(values, function(index, text) {
-        section += '<li>' + text + '</li>';
+
+    $.each(details, function(key, value) {
+      alert('span#' + key + ': ' + value['count']);
+      $('span#' + key).html(value['count']);
+      $.each(value['items'], function(discipline, text_array) {
+        section = '<h3>' + discipline + '</h3><ul>';
+        $.each(text_array, function(index, text) {
+          section += '<li>' + text + '</li>';
+        });
+        section += '</ul>';
+        $('div#' + key + '_list').append(section);
       });
-      section += '</ul>';
-      $('div#conference_list').append(section);
-    });
-    $.each(evnt_detail, function(key, values) {
-      section = '<h3>' + key + '</h3><ul>';
-      $.each(values, function(index, text) {
-        section += '<li>' + text + '</li>';
-      });
-      section += '</ul>';
-      $('div#event_list').append(section);
-    });
-    $.each(hktn_detail, function(key, values) {
-      section = '<h3>' + key + '</h3><ul>';
-      $.each(values, function(index, text) {
-        section += '<li>' + text + '</li>';
-      });
-      section += '</ul>';
-      $('div#hackathon_list').append(section);
-    });
-    $.each(pres_detail, function(key, values) {
-      section = '<h3>' + key + '</h3><ul>';
-      $.each(values, function(index, text) {
-        section += '<li>' + text + '</li>';
-      });
-      section += '</ul>';
-      $('div#project_list').append(section);
-    });
-    $.each(proj_detail, function(key, values) {
-      section = '<h3>' + key + '</h3><ul>';
-      $.each(values, function(index, text) {
-        section += '<li>' + text + '</li>';
-      });
-      section += '</ul>';
-      $('div#project_list').append(section);
     });
   });
 });
